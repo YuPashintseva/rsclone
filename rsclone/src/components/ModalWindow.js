87bec0af435bf5fe0b811e494b5a26b71c440a41
YuPashintseva <juliyapashintseva@gmail.com>
2021-01-11 15:57:22 +0600

    fix: change link router

---
+++ rsclone/src/components/ModalWindow.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function ModalWindow(props) {
+    return (
+        <div class="modal-body">
+            <div class="container-fluid">
+                <div className="row">
+                <div className="col-md-4">.col-md-4</div>
+                <div className="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
+                </div>
+                <div className="row">
+                <div className="col-md-3 ml-auto">.col-md-3 .ml-auto</div>
+                <div className="col-md-2 ml-auto">.col-md-2 .ml-auto</div>
+                </div>
+                <div className="row">
+                <div className="col-md-6 ml-auto">.col-md-6 .ml-auto</div>
+                </div>
+                <div className="row">
+                <div className="col-sm-9">
+                    Level 1: .col-sm-9
+                    <div className="row">
+                    <div className="col-8 col-sm-6">
+                        Level 2: .col-8 .col-sm-6
+                    </div>
+                    <div className="col-4 col-sm-6">
+                        Level 2: .col-4 .col-sm-6
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ModalWindow;
