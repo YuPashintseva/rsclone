306f5c843da092b2b15c1ac07429117d191a5e54
anatkig <70736455+anatkig@users.noreply.github.com>
2021-01-12 01:11:10 +0600

    embeded video plus changed configuration

--- rsclone/src/components/FilmPage.js
+++ rsclone/src/components/FilmPage.js
@@ -5,21 +5,32 @@ class FilmPage extends React.Component{
     constructor(props) {
         super(props);
         this.data = props.value;
-        console.log(this.data);
+        this.state = {video:""};
+      
     }
+    async componentDidMount(){
+     const response = await fetch(`https://api.themoviedb.org/3/movie/${this.data.id}/videos?api_key=fb0fcc2d34caffc53da53d676fbf678a&language=en-US`);
+     const res = await response.json();
+     this.setState({ video: res.results[0].key});
+     console.log(this.state)
+    }
+    
     render(){
+       
         return(
-            <div id = "fp" className="wrapper wrapperStyle">
+            
+            <div id = "fp" className=" container-fluid wrapper wrapperStyle">
                
                <div id = "upperBlock" className="wrapper">
                  <div className="row"></div>
-                 <div className="row">Main Info:
+                 <div className="row">
 
                      <h1 className="col">{this.data.original_title}</h1>
-                     <h3 className="col">Average Vote: {this.data.vote_average}
+                     <div className="col">
+                     <h3 className="row">Average Vote: {this.data.vote_average}
                      <img src={star}></img>
                      </h3>
-                      
+                     </div>
                      <h3 className="col">Votes: {this.data.vote_count}</h3>
                  </div>
                </div>
@@ -27,14 +38,14 @@ class FilmPage extends React.Component{
                <div id = "briefInfo" className="wrapper">
                    <div>Release Date: {this.data.release_date}</div>
                    <div></div>
-                   </div>
+                </div>
 
-               <div id = "photoVideo"className="row">
+               <div id = "photoVideo" className="row" >
                    
-               <img width="75%" height ="auto" src={`https://image.tmdb.org/t/p/original/${this.data.poster_path}`} alt={this.data.title}/>
-                <div style={{ maxWidth: "20%", margin: "8px"}}><h3 >Synopsis: {this.data.overview}</h3></div>
+               <img  width="40%"height ="auto" src={`https://image.tmdb.org/t/p/original/${this.data.poster_path}`} alt={this.data.title}/>
+               <iframe  width="60%" height ="100%" src={`https://www.youtube.com/embed/${this.state.video}`}></iframe>
                </div>
-
+               <div style={{  margin: "8px"}}><h3 >Synopsis: {this.data.overview}</h3></div>
              
                <div id = "videoCarousel" className="wrapper"></div>
                <div id = "photoCarousel" className="wrapper"></div>
