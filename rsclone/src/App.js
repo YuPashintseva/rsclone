442df5296bb267b4f6bf6959bb5ac7b979cf3ad4
Vagiz Musin <vagiz_mus@mail.ru>
2021-01-14 01:39:22 +0600

    feat: add search and change a little main page app

--- rsclone/src/App.js
+++ rsclone/src/App.js
@@ -1,68 +1,129 @@
-import ReactDOM from 'react-dom';
-import  FilmList  from "./components/FilmList";
+import ReactDOM from "react-dom";
+import FilmList from "./components/FilmList";
 import FilmPage from "./components/FilmPage";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import MovieRow from "./components/MovieRow";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import $ from "jquery";
 
-
-
-function App() {
+class App extends Component {
   //console.log(sessionStorage.getItem("val"))
- 
-  return (
-    <div className="App">
-<Router>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{margin:"24px 0"}}>
-  <a className="navbar-brand" href="javascript:void(0)">Logo</a>
-  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  
-  <div className="collapse navbar-collapse" id="navb">
-   
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.performSearch("");
+  }
 
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-      <Link to='/FilmPage'> </Link>
-      <Link to="/">  <a className="nav-link" >Home</a></Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link">Link</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled">Disabled</a>
-      </li>
-    </ul>
-   
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
-      <button className="btn btn-success my-2 my-sm-0" type="button">Search</button>
-    </form>
-    </div>
- </nav>
+  performSearch(searchTerm) {
+    const urlString =
+      "https://api.themoviedb.org/3/search/movie?api_key=c9ebd652172bbcdaa5b3746fa2e60207&query=" +
+      searchTerm;
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully");
+        console.log(searchResults);
+        const results = searchResults.results;
+        // console.log(results[0]);
 
-      <div id="ourRoot" className="d-flex justify-content-around">
-        <div id="fl" className="films-list">
-        <Route exact path="/" ><FilmList/></Route>
-    
-        <Route path="/FilmPage"><FilmPage/></Route>
-       
-        
-        </div>
+        var movieRows = [];
+
+        results.forEach((movie) => {
+          movie.poster_path =
+            "https://image.tmdb.org/t/p/w600_and_h900_bestv2" +
+            movie.poster_path;
+          console.log(movie.poster_path);
+          const movieRow = <MovieRow key={movie.id} movie={movie} />;
+          movieRows.push(movieRow);
+        });
+
+        this.setState({ rows: movieRows });
+      },
+      error: (xhr, status, err) => {
+        console.log("failed to fetch data");
+      },
+    });
+  }
+
+  searchCnangeHandler(event) {
+    console.log(event.target.value);
+    const boundObject = this;
+    const searchTerm = event.target.value;
+    boundObject.performSearch(searchTerm);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <nav
+            className="navbar navbar-expand-lg navbar-dark bg-dark"
+            style={{ margin: "24px 0" }}
+          >
+            <a className="navbar-brand" href="javascript:void(0)">
+              Logo
+            </a>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navb"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navb">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/FilmPage"> </Link>
+                  <Link to="/">
+                    {" "}
+                    <a className="nav-link">Home</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  onChange={this.searchCnangeHandler.bind(this)}
+                  type="text"
+                  list="datalistOptions"
+                  id="exampleDataList"
+                  placeholder="Search"
+                />
+                <button className="btn btn-success my-2 my-sm-0" type="button">
+                  Search
+                </button>
+                <datalist id="datalistOptions">{this.state.rows}</datalist>
+              </form>
+            </div>
+          </nav>
+
+          <div id="ourRoot" className="d-flex justify-content-around">
+            <div id="fl" className="films-list">
+              <Route exact path="/">
+                <FilmList />
+              </Route>
+
+              <Route path="/FilmPage">
+                <FilmPage />
+              </Route>
+            </div>
+          </div>
+        </Router>
       </div>
-</Router>
-    </div>
-    
-  ); 
+    );
+  }
 }
-
 
 /*
 export function filmPage(el) {
