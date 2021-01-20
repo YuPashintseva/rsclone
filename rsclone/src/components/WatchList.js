30515c95a4c0d2f70b41e9816dd4b548b447d766
YuPashintseva <juliyapashintseva@gmail.com>
2021-01-20 12:27:57 +0600

    add close btn

--- rsclone/src/components/WatchList.js
+++ rsclone/src/components/WatchList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Modal, Container, Row, Col, Image } from 'react-bootstrap';
+import close from './assets/close.png'
 
 
 class Watchlist extends React.Component{
@@ -13,7 +14,7 @@ class Watchlist extends React.Component{
                 <div id = "fp" className="container-fluid wrapperStyle">
                     <div className="top-menu-wrapper">
                         <div className="head-text">Your Watchlist</div>
-                        <button className="back-button" >Back</button>
+                        <button className="back-button">Home</button>
                     </div>
                     <div>
                     {filmsList.map(el => (
@@ -21,11 +22,14 @@ class Watchlist extends React.Component{
                             <Col xs={6} md={4}>
                                 {el[0] ? <Image className="poster-img-modal img-watchlist" src={`https://image.tmdb.org/t/p/original/${el[0]}`} alt={el[0]} thumbnail />: null}
                             </Col>
-                            <Col xs={12} md={8}>
+                            <Col xs={11} md={7}>
                                 {el[3] ? <div><strong>Title: </strong> {el[3]}</div> : null}
                                 {el[1] ? <div><strong>Release date: </strong>  {el[1]}</div> : null}
                                 {el[2] ? <div><strong>Stars: </strong> {el[2]}</div> : null}
                                 {el[4] ? <div><strong>Overview: </strong> {el[4]}</div> : null}
+                            </Col>
+                            <Col xs={1} md={1}>
+                                {el[0] ? <div className="background-close-btn"><Image className="close-btn-img" src={close} alt="close button" /></div> : null}
                             </Col>
                         </Row>
                         
