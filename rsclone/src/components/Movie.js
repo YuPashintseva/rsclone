67d3e8e7ac4a677c9255f999faf113a1a1f90841
anatkig <70736455+anatkig@users.noreply.github.com>
2021-01-31 09:14:16 +0600

    improved star rotation

--- rsclone/src/components/Movie.js
+++ rsclone/src/components/Movie.js
@@ -13,7 +13,7 @@ const Movie = (props) => {
 
           <h4 className="col-auto">
             Average Vote: {props.vote_average}
-            <img src={star}></img>
+            <img className="star" src={star}></img>
           </h4>
 
           <h4 className="col-auto">Votes: {props.vote_count}</h4>
